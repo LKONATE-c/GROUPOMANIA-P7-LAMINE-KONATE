2d3ce8b2c3d9c0a2b037ebe2e0c7eeea47f1ffa1
LKONATE <lkonate78@yahoo.com>
2022-03-17 22:21:45 +0100

    formate du code

--- frontend/src/components/User.vue
+++ frontend/src/components/User.vue
@@ -1,92 +1,75 @@
 <template>
-<h1>User list</h1>
-<section>
-    
+  <h1>User list</h1>
+  <section>
     <div class="table-responsive col-lg-12">
-        <table class="table table-light table-striped table-hover table-borderless">
-                <thead class="table-dark">
-                    <tr>
-                            <th scope="col">id</th>
-                            <th scope="col">First Name</th>
-                            <th scope="col">Last Name</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Action</th>
-                    </tr>
-                </thead>
-                 <tbody>
-                     <tr v-for="user in users" v-bind:key="user.id">
-                         <td>{{user.id}}</td>
-                         <td>{{user.firstname}}</td>
-                         <td>{{user.lastname}}</td>
-                         <td>{{user.email}}</td>
-                         <td button v-on:click="deleteUser(user.id)">Delete</td>
-                     </tr>
-
-                 </tbody>
-
-        </table>
-
-
+      <table
+        class="table table-light table-striped table-hover table-borderless"
+      >
+        <thead class="table-dark">
+          <tr>
+            <th scope="col">id</th>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="user in users" v-bind:key="user.id">
+            <td>{{ user.id }}</td>
+            <td>{{ user.firstname }}</td>
+            <td>{{ user.lastname }}</td>
+            <td>{{ user.email }}</td>
+            <td button v-on:click="deleteUser(user.id)">Delete</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-</section>
-  
+  </section>
 </template>
 
 <script>
 import axios from "axios";
 
-
 export default {
-    name:"User",
-    data () {
-        return { user:null
-            
-        }
+  name: "User",
+  data() {
+    return { user: null };
+  },
+  methods: {
+    //recupération des users
+    getData() {
+      axios.get("/api/user/getone/" + userid).then((result) => {
+        console.log(result);
+        this.user = result.user;
+      });
+    }, //supression des users
+    deleteUser(id) {
+      axios.delete("api/user/" + id).then(() => {
+        this.getData();
+      });
     },
-    methods: {
-
-        getData() {
-            axios
-            .get("/api/user/getone/"+userid)
-            .then((result)=>{
-                console.log(result)
-                this.user=result.user
-
-            })
-        },
-        deleteUser(id) {
-            axios
-             .delete("api/user/"+id)
-             .then(()=>{
-                 this.getData();
-
-             })
-        }
-
-    },
-    mounted(){
-        this.getData()
-    }
-
-
-}
+  },
+  mounted() {
+    this.getData();
+  },
+};
 </script>
 
 <style>
-    section {
-        background-color: #000000cc;
-        width: 100%;
-        min-height: 100vh;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        padding: 100px;
-    }
-
-    @media screen and (max-width: 768px) {
-    section {
-        padding: 50px;
-    }
+section {
+  background-color: #000000cc;
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 100px;
 }
 
+@media screen and (max-width: 768px) {
+  section {
+    padding: 50px;
+  }
+}
 </style>
